d49df7dbe6eddd7f692216fb6a3b8d47c39cc90a
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-05-01 18:23:06 +0300

    implemented searchbar functionality

---
+++ src/widgets/SearchBar/model/searchSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit"
+import type { PayloadAction } from "@reduxjs/toolkit"
+
+import { TRepoInfo } from "../../../lib"
+import { RootState } from "../../../shared/store"
+
+const initialState = {
+  url: "",
+}
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setUrl: (state, action: PayloadAction<string>) => {
+      state.url = action.payload
+    },
+  },
+})
+
+export const { setUrl } = searchSlice.actions
+
+export const selectUrl = (state: RootState) => state.search.url
+export const selectRepoInfo = (state: RootState): TRepoInfo => {
+  const [name, repo] = state.search.url.split("/").slice(-2)
+
+  // if (!name || !repo) {
+  //   throw new Error(
+  //     "Provided url does not contain either profile name or repository"
+  //   )
+  // }
+
+  return {
+    name,
+    repo,
+  }
+}
+
+export const searchReducer = searchSlice.reducer
---
+++ src/widgets/SearchBar/ui/SearchBar/SearchBar.tsx
@@ -0,0 +1,58 @@
+import { Input } from "antd"
+import { FC, useCallback } from "react"
+import { ToastContainer } from "react-toastify"
+
+import { setUrl } from "../.."
+import { useAppDispatch, useToast, validateUrl } from "../../../../shared/lib"
+
+const { Search } = Input
+
+const searchSuggestions: string[] = [
+  "facebook/react",
+  "vuejs/core",
+  "angular/angular",
+  "remix-run/react-router",
+  "reduxjs/redux-toolkit",
+].map((i) => `https://github.com/${i}`)
+
+export const SearchBar: FC = () => {
+  const dispatch = useAppDispatch()
+
+  const fireErrorToast = useToast("Provided url is invalid")
+
+  const onSearch = useCallback(
+    (value: string) => {
+      const isValidUrl = validateUrl(value)
+
+      if (!isValidUrl) {
+        fireErrorToast()
+        return
+      }
+
+      dispatch(setUrl(value))
+    },
+    [dispatch, fireErrorToast]
+  )
+
+  return (
+    <>
+      <Search
+        placeholder="Enter repo URL"
+        enterButton="Load issues"
+        size="middle"
+        allowClear={true}
+        defaultValue="https://github.com/facebook/react"
+        bordered={true}
+        loading={false}
+        onSearch={onSearch}
+        list="search-suggestions"
+      />
+      <datalist id="search-suggestions">
+        {searchSuggestions.map((item) => (
+          <option key={item} value={item} />
+        ))}
+      </datalist>
+      <ToastContainer />
+    </>
+  )
+}
