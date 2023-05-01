43c0f11c06aa6433b5806efc15e500c68da89184
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-05-01 17:54:07 +0300

    started implementing feature sliced architecture

--- src/api/apiSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-
-import { baseUrl } from "../lib/consts"
-
-export const api = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl }),
-  endpoints: () => ({}),
-})
