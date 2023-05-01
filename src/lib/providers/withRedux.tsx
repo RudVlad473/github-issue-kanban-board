425a4c631295af8d502b836d2692a6b8403dad4e
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-05-01 18:22:52 +0300

    moved providers

--- src/lib/providers/withRedux.tsx
+++
@@ -1,6 +0,0 @@
-import { Provider } from "react-redux"
-
-import { store } from "../store/store"
-
-export const withRedux = (component: () => React.ReactNode) => () =>
-  <Provider store={store}>{component()}</Provider>
